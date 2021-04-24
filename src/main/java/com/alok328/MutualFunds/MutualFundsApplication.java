38d7c35370c841aff33004ce8249c1032a42a780
alok328 <40838760+alok328@users.noreply.github.com>
2021-04-24 10:21:10 +0530

    initialized project

---
+++ src/main/java/com/alok328/MutualFunds/MutualFundsApplication.java
@@ -0,0 +1,13 @@
+package com.alok328.MutualFunds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MutualFundsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MutualFundsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alok328/MutualFunds/MutualFundsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alok328.MutualFunds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MutualFundsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
