e5a2a7bc320ddbf9ca28a926e907f2c5a1e55ad9
alok328 <alok328raj@gmail.com>
2021-04-24 11:24:00 +0530

    initialized struct

---
+++ src/main/java/com/alok328/MutualFunds/controller/DUMMY.java
@@ -0,0 +1,5 @@
+package com.alok328.MutualFunds.controller;
+
+public class DUMMY {
+    //TODO: refactor as required
+}
---
+++ src/main/java/com/alok328/MutualFunds/repository/DUMMY.java
@@ -0,0 +1,5 @@
+package com.alok328.MutualFunds.repository;
+
+public class DUMMY {
+    //TODO: refactor as required
+}
